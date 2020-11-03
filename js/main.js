ae9136de257cabed3e9f143116a2831f12615013
Javi <ejviquin@gmail.com>
2020-11-02 19:43:00 -0500

    desde ubuntu

--- js/main.js
+++ js/main.js
@@ -1,26 +1,41 @@
-		//Render
-		let $pais = document.getElementById('pais'); 
-		let $contenido = document.getElementById('contenido');
-		let $dNConfirm =document.getElementById('dNConfirm');
-		let $dTDeaths =document.getElementById('dTDeaths');
-		let $dTRecovered =document.getElementById('dTRecovered');
+// btn Peticion
 
-		function renderText(html, t){
-			html.textContent = t
+let $btn = document.getElementById('btnCode'); 
+
+$btn.addEventListener("click", () => {
+		
+		let numberPais = document.getElementById("numberPais").value;
+		console.log(numberPais);
+		if(numberPais>189){
+			alert('No hay tantos paises datados!')
 		}
 
-			//Peticion
-		fetch('https://api.covid19api.com/summary')
-		.then(covidData => covidData.json())
-		.then(pais => pais.Countries[36])//Colombia = 36
-		.then(data => {
-			debugger
-			renderText($pais, data.Country)
-			renderText($contenido, data.TotalConfirmed)
-			
-			renderText($dNConfirm, data.NewConfirmed)
-			renderText($dTDeaths, data.TotalDeaths)
-			renderText($dTRecovered, data.TotalRecovered)
 
-		})
-		.catch(e => console.log(e));	
+
+	//Render
+
+	let $pais = document.getElementById('pais'); 
+	let $contenido = document.getElementById('contenido');
+	let $dNConfirm =document.getElementById('dNConfirm');
+	let $dTDeaths =document.getElementById('dTDeaths');
+	let $dTRecovered =document.getElementById('dTRecovered');
+
+		const renderText = (html, t) => html.textContent = t
+
+		//Peticion
+	fetch('https://api.covid19api.com/summary')
+	.then(covidData => covidData.json())
+	.then(pais => pais.Countries[numberPais])//Colombia = 36
+	.then(data => {
+		
+		renderText($pais, data.Country)
+		renderText($contenido, data.TotalConfirmed)
+		renderText($dNConfirm, data.NewConfirmed)
+		renderText($dTDeaths, data.TotalDeaths)
+		renderText($dTRecovered, data.TotalRecovered)
+
+
+	})
+	.catch(e => console.log(e));	
+
+});
